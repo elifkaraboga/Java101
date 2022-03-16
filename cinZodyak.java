5c193f3b93d8ccc91e0606f3d9c48f257f82225c
Elif Karaboğa <54960657+elifkaraboga@users.noreply.github.com>
2022-03-16 22:57:30 +0300

    Add files via upload

---
+++ cinZodyak.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class cinZodyak {
+
+    public static void main(String[] args) {
+
+
+        Scanner inp = new Scanner(System.in);
+
+        int yil;
+
+        System.out.println("Doğum yılınızı giriniz: ");
+        yil = inp.nextInt();
+
+
+        if (yil % 12 == 0 ) {
+            System.out.print("Çin Zodyağı Burcunuz : Maymun");
+        }else if (yil % 12 == 1) {
+            System.out.print("Çin Zodyağı Burcunuz : Horoz ");
+
+        }else if (yil % 12 == 2) {
+            System.out.print("Çin Zodyağı Burcunuz : Köpek ");
+
+        }else if (yil % 12 == 3) {
+            System.out.print("Çin Zodyağı Burcunuz : Domuz ");
+
+        }else if (yil % 12 == 4) {
+            System.out.print("Çin Zodyağı Burcunuz : Fare ");
+
+        }else if (yil % 12 == 5) {
+            System.out.print("Çin Zodyağı Burcunuz : Öküz ");
+
+        }else if (yil % 12 == 6) {
+            System.out.print("Çin Zodyağı Burcunuz : Kaplan ");
+
+        }else if (yil % 12 == 7) {
+            System.out.print("Çin Zodyağı Burcunuz : Tavşan ");
+
+        }else if (yil % 12 == 8) {
+            System.out.print("Çin Zodyağı Burcunuz : Ejderha ");
+
+        }else if (yil % 12 == 9) {
+            System.out.print("Çin Zodyağı Burcunuz : Yılan ");
+
+        }else if (yil % 12 == 10) {
+            System.out.print("Çin Zodyağı Burcunuz : At ");
+
+        }else if (yil % 12 == 11) {
+            System.out.print("Çin Zodyağı Burcunuz : koyun ");
+        }
+
+
+
+
+
+    }
+}
