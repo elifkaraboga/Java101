0c041c0b4923f801c7457ae080ba7b26bcb23550
Elif Karaboğa <54960657+elifkaraboga@users.noreply.github.com>
2022-03-16 23:16:43 +0300

    Add files via upload

---
+++ artikyilHesapla.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class artikyilHesapla {
+
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+
+        int yil, kalan;
+
+        Scanner girdi = new Scanner(System.in);
+
+        System.out.print("Yil Giriniz: ");
+        yil = girdi.nextInt();
+
+        if(yil % 4 == 0){
+            if(yil % 100 != 0){
+                System.out.println(yil + " Artik Yildir");
+            }
+            else if((yil % 100 == 0) && (yil % 400 == 0)){
+                System.out.println(yil + " Artik Yildir");
+            }else{
+                System.out.println(yil + " Artik Yil Degildir.");
+            }
+        }
+        else{
+            System.out.println(yil + " Artik Yil Degildir.");
+        }
+    }
+}
